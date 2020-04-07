fca39f238a331abb02a442a177f159fa521fe728
Chad Schultz <itoleck@hotmail.com>
2020-04-07 14:56:15 -0400

    1.0.10 Updates

--- AzureSM-Login-UpdateModule.ps1
+++ AzureSM-Login-UpdateModule.ps1
@@ -1,6 +1,6 @@
 ﻿#Azure login and module updating
 
-Set-Location -Path %USERPROFILE%\source\repos\AzureSaveMoney
+Set-Location -Path $env:USERPROFILE\source\repos\AzureSaveMoney
 Import-Module .\AzureSaveMoney.psm1 -Verbose -Force -Global
 
 Login-AzAccount
--- AzureSaveMoney.psm1
+++ AzureSaveMoney.psm1
@@ -91,6 +91,87 @@ function global:Get-AzSMUnusedNSGs {
 	
   Return $nsg
 }
+
+function global:Get-AzSMEmptySubnets {
+
+  <#
+      .SYNOPSIS
+      Lists empty subnets in a subscription.
+      .DESCRIPTION
+      Lists empty subnets in a subscription.
+      .PARAMETER SubscriptionID
+      Azure subscription ID in the format, 00000000-0000-0000-0000-000000000000
+      .OUTPUTS
+      Selected.Microsoft.Azure.Commands.Network.Models.PSVirtualNetwork
+      .EXAMPLE
+      Get-AzSMEmptySubnets -Subscription 00000000-0000-0000-0000-000000000000
+      Get a list of unused subnets in a subscription.
+      .EXAMPLE
+      *
+      .NOTES
+      *
+      *
+      .LINK
+  #>
+
+  [CmdletBinding(
+      DefaultParameterSetName='SubscriptionID',
+      ConfirmImpact='low'
+  )]
+
+  param(
+    [Parameter(Mandatory=$true)][string] $SubscriptionID
+  )
+
+    $null = Set-AzContext -SubscriptionId $SubscriptionID
+  Write-Debug ('Subscription: {0}' -f $SubscriptionID)
+
+  $vn=Get-AzVirtualNetwork
+  $emptysubnets=$vn|?{$_.Subnets.IpConfigurations.count -eq 0}|select-object @{n="VNet";e="Name"},Subnets
+  
+  Return $emptysubnets
+}
+
+function global:Get-AzSMUnusedAppServicePlans {
+
+  <#
+      .SYNOPSIS
+      Lists unused App Service Plans in a subscription.
+      .DESCRIPTION
+      Lists unused App Service Plans in a subscription.
+      .PARAMETER SubscriptionID
+      Azure subscription ID in the format, 00000000-0000-0000-0000-000000000000
+      .OUTPUTS
+      Microsoft.Azure.Commands.WebApps.Models.WebApp.PSAppServicePlan
+      .EXAMPLE
+      Get-AzSMUnusedAppServicePlans -Subscription 00000000-0000-0000-0000-000000000000
+      Get a list of unused App Service Plans in a subscription.
+      .EXAMPLE
+      Get-AzSMUnusedAppServicePlans -Subscription 00000000-0000-0000-0000-000000000000 | Remove-AzAppServicePlan
+      Remove unused App Service Plans in a subscription.
+      .NOTES
+      * CAN be piped to Remove-AzAppServicePlan.
+      * When piping to remove resources, include the -force parameter to supress prompts.
+      .LINK
+  #>
+
+  [CmdletBinding(
+      DefaultParameterSetName='SubscriptionID',
+      ConfirmImpact='low'
+  )]
+
+  param(
+    [Parameter(Mandatory=$true)][string] $SubscriptionID
+  )
+
+    $null = Set-AzContext -SubscriptionId $SubscriptionID
+  Write-Debug ('Subscription: {0}' -f $SubscriptionID)
+
+  $app=Get-AzAppServicePlan|Where-Object{$_.NumberOfSites -eq 0}
+	
+  Return $app
+}
+
 function global:Get-AzSMUnusedPIPs {
 
   <#
@@ -1047,4 +1128,10 @@ function global:Get-AzSMAllResources {
   
     Write-Output 'VM CPU scaling info'
     Get-AzSMVMScaleinfo -SubscriptionID $SubscriptionID
+
+    Write-Output 'Empty Subnets'
+    Get-AzSMEmptySubnets -SubscriptionID $SubscriptionID
+  
+    Write-Output 'Unused App Service Plans'
+    Get-AzSMUnusedAppServicePlans -SubscriptionID $SubscriptionID
 }
